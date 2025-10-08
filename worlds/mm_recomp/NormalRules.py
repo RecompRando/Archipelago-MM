fcec992281bb1b1170b0c7017cd58ce03dc5983a
PixelShake92 <shane.bell.music@gmail.com>
2025-10-09 09:16:55 +1100

    Update HSW logic

--- worlds/mm_recomp/NormalRules.py
+++ worlds/mm_recomp/NormalRules.py
@@ -74,7 +74,7 @@ def can_get_cow_milk(state, player):
               can_use_light_arrows(state, player) and 
               (state.can_reach("Twin Islands Hot Water Grotto Chest", 'Location', player) or 
                (state.has("Goron Mask", player) and 
-                state.can_reach("Mountain Village Healing Darmani", 'Location', player)) or 
+                can_use_lens(state, player)) or 
                state.can_reach("Ikana Well Invisible Chest", 'Location', player)))))
 
 def has_bottle(state, player, need_count=1):
@@ -1110,7 +1110,7 @@ def get_location_rules(player, options, prices):
                     can_use_fire_arrows(state, player) or 
                     (
                         (
-                            state.can_reach("Mountain Village Healing Darmani", 'Location', player) or 
+                            can_use_lens(state, player) or 
                             (
                                 state.can_reach("Ikana Well Invisible Chest", 'Location', player) and 
                                 can_play_song("Song of Soaring", state, player)
@@ -1127,7 +1127,7 @@ def get_location_rules(player, options, prices):
                     can_use_fire_arrows(state, player)
                 ) or 
                 (
-                    state.can_reach("Mountain Village Healing Darmani", 'Location', player) and 
+                    can_use_lens(state, player) and 
                     has_bottle(state, player) and 
                     state.has("Goron Mask", player) and 
                     has_explosives(state, player)
@@ -2138,7 +2138,7 @@ def get_location_rules(player, options, prices):
                                     can_use_fire_arrows(state, player)
                                 ) or 
                                 (
-                                    state.can_reach("Mountain Village Healing Darmani", 'Location', player) and 
+                                    can_use_lens(state, player) and 
                                     state.has("Goron Mask", player)
                                 )
                             )
