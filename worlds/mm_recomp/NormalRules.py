7b81e80bd27be50f2bf3d5c4164e867f4df57ea4
ThatHypedPerson <thathypedperson@gmail.com>
2025-10-24 00:30:15 -0700

    add options to lower token and fairy requirements

--- worlds/mm_recomp/NormalRules.py
+++ worlds/mm_recomp/NormalRules.py
@@ -862,7 +862,7 @@ def get_location_rules(player, options, prices):
                 has_projectiles(state, player)
             ),
         "Swamp Spider House Reward":
-            lambda state: state.has("Swamp Skulltula Token", player, 30),
+            lambda state: state.has("Swamp Skulltula Token", player, options.required_skull_tokens.value),
 
         "Deku Palace Bean Seller":
             lambda state: state.has("Deku Mask", player),
@@ -891,7 +891,7 @@ def get_location_rules(player, options, prices):
             ),
 
         "Woodfall Great Fairy Reward":
-            lambda state: state.has("Stray Fairy (Woodfall)", player, 15),
+            lambda state: state.has("Stray Fairy (Woodfall)", player, options.required_stray_fairies.value),
         "Woodfall Owl Statue Chest":
             lambda state: state.has("Deku Mask", player),
         "Woodfall Bridge Chest":
@@ -1254,7 +1254,7 @@ def get_location_rules(player, options, prices):
             ),
             
         "Snowhead Great Fairy Reward":
-            lambda state: state.has("Stray Fairy (Snowhead)", player, 15),
+            lambda state: state.has("Stray Fairy (Snowhead)", player, options.required_stray_fairies.value),
             
         "Snowhead Temple Bridge Room Under Platform Bubble SF":
             lambda state: (
@@ -1716,7 +1716,7 @@ def get_location_rules(player, options, prices):
                 has_explosives(state, player)
             ),
         "Ocean Spider House Reward":
-            lambda state: state.has("Ocean Skulltula Token", player, 30),
+            lambda state: state.has("Ocean Skulltula Token", player, options.required_skull_tokens.value),
         
         "Pirates' Fortress Exterior Underwater Log Chest":
             lambda state: state.has("Zora Mask", player),
@@ -1840,7 +1840,7 @@ def get_location_rules(player, options, prices):
 
         "Great Bay Great Fairy Reward":
             lambda state: (
-                state.has("Stray Fairy (Great Bay)", player, 15) and 
+                state.has("Stray Fairy (Great Bay)", player, options.required_stray_fairies.value) and 
                 state.has("Hookshot", player)
             ),
             
@@ -2050,7 +2050,7 @@ def get_location_rules(player, options, prices):
 
         "Stone Tower Great Fairy Reward":
             lambda state: (
-                state.has("Stray Fairy (Stone Tower)", player, 15) and 
+                state.has("Stray Fairy (Stone Tower)", player, options.required_stray_fairies.value) and 
                 can_use_ice_arrows(state, player)
             ),
             
