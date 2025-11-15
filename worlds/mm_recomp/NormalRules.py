c08cdb1035594a9a5d7a3b04b4e1855590d9fbbd
LittleCube <littlecubehax@gmail.com>
2025-11-14 23:28:00 -0500

    remove duplicate sign locations for now

--- worlds/mm_recomp/NormalRules.py
+++ worlds/mm_recomp/NormalRules.py
@@ -19333,24 +19333,24 @@ def get_location_rules(player, options):
             lambda state: True,
         #"Southern Swamp (Clear State) Witch's Hut Cut the Sign":
            # lambda state: can_clear("Woodfall", state, player),
-        "Woods of Mystery Cut the Sign Day 1 (1)":
+        "Woods of Mystery Cut the Sign Day 1":
             lambda state: True,
-        "Woods of Mystery Cut the Sign Day 1 (2)":
+        # ~ "Woods of Mystery Cut the Sign Day 1 (2)":
+            # ~ lambda state: True,
+        # ~ "Woods of Mystery Cut the Sign Day 1 (3)":
+            # ~ lambda state: True,
+        "Woods of Mystery Cut the Sign Day 2":
             lambda state: True,
-        "Woods of Mystery Cut the Sign Day 1 (3)":
+        # ~ "Woods of Mystery Cut the Sign Day 2 (5)":
+            # ~ lambda state: True,
+        # ~ "Woods of Mystery Cut the Sign Day 2 (6)":
+            # ~ lambda state: True,
+        "Woods of Mystery Cut the Sign Day 3":
             lambda state: True,
-        "Woods of Mystery Cut the Sign Day 2 (4)":
-            lambda state: True,
-        "Woods of Mystery Cut the Sign Day 2 (5)":
-            lambda state: True,
-        "Woods of Mystery Cut the Sign Day 2 (6)":
-            lambda state: True,
-        "Woods of Mystery Cut the Sign Day 3 (7)":
-            lambda state: True,
-        "Woods of Mystery Cut the Sign Day 3 (8)":
-            lambda state: True,
-        "Woods of Mystery Cut the Sign Day 3 (9)":
-            lambda state: True,
+        # ~ "Woods of Mystery Cut the Sign Day 3 (8)":
+            # ~ lambda state: True,
+        # ~ "Woods of Mystery Cut the Sign Day 3 (9)":
+            # ~ lambda state: True,
 
         "Southern Swamp Log Cut the Sign":
             lambda state: True,
